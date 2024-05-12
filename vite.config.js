ddefc70e7714edfad7caa6098fb3db7f197f2696
Данил Рузин <ruzin-de@yandex.ru>
2024-05-12 21:57:44 +0500

    Доработан dockerfile и viteconfig для сборки через docker

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const backend = 'backend'
+const backend = 'http://localhost:9051'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [react()],
 	server: {
 		host: '0.0.0.0',
-		port: 5173,
 		proxy: {
 			'^/notes': {
-				target: backend,
+				target: mode == 'development' ? backend : 'http://backend:8080',
 				ws: false,
 				secure: false,
 			},
 		},
 	},
-})
+}))
